95f9cce9c746f724a80e8db21c73af21f45bf8c2
junokims <juno.jaekwon.kim@gmail.com>
2020-09-05 17:47:18 -0700

    removed upper level directory

---
+++ build/precache-manifest.41122c6a2374e48e2daa14597189ada9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a349c4fc61b3b33b317f8e4df47e3ce",
+    "url": "./public/index.html/index.html"
+  },
+  {
+    "revision": "607ff6e831e53232e811",
+    "url": "./public/index.html/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "270d97f6ddfeb076c295",
+    "url": "./public/index.html/static/js/2.660f1340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./public/index.html/static/js/2.660f1340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607ff6e831e53232e811",
+    "url": "./public/index.html/static/js/main.e0ca6b5c.chunk.js"
+  },
+  {
+    "revision": "0e1e1644a872ec9a5228",
+    "url": "./public/index.html/static/js/runtime-main.0070ebc9.js"
+  }
+]);
